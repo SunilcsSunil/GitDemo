cd7fe4af34281c3bb64bda4f10ade3dcdcbd7933
sunil <sunilcs703@gmail.com>
2024-01-28 16:54:55 +0530

    american development team

--- src/main/java/qaclickacademy/demo.java
+++ src/main/java/qaclickacademy/demo.java
@@ -1,5 +1,9 @@
 package qaclickacademy;
 
 public class demo {
+	public static void main(String[] args) {
+		System.out.println("git american team ");
+		System.out.println("changed some code ");
+	}
 
 }
